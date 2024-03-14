a114c8887bc3375993c0952d295169d4eafebcba
G_Leitao18 <G_Leitao18@cranford.hounslow.sch.uk>
2024-03-14 11:28:27 +0000

    added print line added more things

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fpdf import FPDF
 pdf = FPDF(orientation="P" , unit="mm" , format="A4")
 
 pdf.add_page()
-
+pdf.set_font(family="Arial", style="B" , size=12)
+pdf.cell(w=0, h=12 , txt="tile of page 1", align="L", ln=1, border=1)
 pdf.output("output.pdf")
 
-print("hello")
+
